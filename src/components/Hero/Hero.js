09c3955f02f919e35d76145f527ec8f805c880de
David Silveira <davidsilveira.3.djs@gmail.com>
2024-11-06 15:20:17 -0500

    changed para

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
@@ -12,10 +16,15 @@ const Hero = (props) => (
         Full Stack Software Engineer
       </SectionTitle>
       <SectionText>
-        As a seasoned full-stack web developer with a background in Neuroscience and a Computer Science minor, I offer a unique perspective on user web experiences, honed through professional experience in developing enterprise applications. My strong problem-solving skills and keen sense of timing enable me to meet project deadlines effectively, balancing a clear vision of broader objectives with meticulous attention to detail for innovative strategy implementation.
-
+        As a software engineer with a background in Neuroscience and a Computer
+        Science, I offer a unique perspective on user web experiences, honed
+        through professional experience in developing enterprise applications.
+        My strong problem-solving skills and keen sense of timing enable me to
+        meet project deadlines effectively, balancing a clear vision of broader
+        objectives with meticulous attention to detail for innovative strategy
+        implementation.
       </SectionText>
-      <Button onClick={() => (window.location = "#footer")}>Contact Me</Button>
+      <Button onClick={() => (window.location = '#footer')}>Contact Me</Button>
     </LeftSection>
   </Section>
 );
